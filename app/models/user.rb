a34237681c754c0b05120d1afb7198b958211c1f
agrumes <gratitude2016@icloud.com>
2022-02-15 23:05:05 +0900

    user登録のvalidatesの追加、エラーメッセージの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:"is invalid. Include both letters and numbers"}
   validates :birthday, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters"}
+  validates :last_name_kana, presence: true, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "is invalid. Input full-width katakana characters"}
+  validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,  message: "is invalid. Input full-width katakana characters"}
 end
