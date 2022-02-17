d0ad773de25580c4d51a5dc0acf9667899cf19ae
agrumes <gratitude2016@icloud.com>
2022-02-17 12:40:36 +0900

    単体テスト用gemのインストール及びベースの記述

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do 
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+    
+    
+    
+    
+    end
+    context '新規登録できない場合' do
+    
+    
+    
+    
+    
+    end
+  end 
+end
