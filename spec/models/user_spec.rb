d9b2b30879705bf6033a5b8d9964874e277ac2b7
agrumes <gratitude2016@icloud.com>
2022-02-17 23:09:32 +0900

    exampleの整理、記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,17 +7,44 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-    
-    
-    
-    
+      it 'nickname、email、passwprd、password_confirmation、last_name、first_name、last_name_kana、first_name_kana、birthdayが存在すれば登録できる' do
+      end
+      it 'passwordが6文字以上であれば登録できる' do
+      end
     end
     context '新規登録できない場合' do
-    
-    
-    
-    
-    
+      it 'nicknameが空では登録できない' do
+      end
+      it 'emailが空では登録できない' do
+      end
+      it '重複したemailが存在する場合は登録できない' do
+      end
+      it 'passwordが空では登録できない' do
+      end
+      it 'passwordが5文字以下であれば登録できない' do
+      end
+      it 'password_confirmationが空では登録できない' do
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+      end
+      it 'last_nameが空では登録できない' do
+      end
+      it 'first_nameが空では登録できない' do
+      end
+      it 'last_name_kanaが空では登録できない' do
+      end
+      it 'first_name_kanaが空では登録できない' do
+      end
+      it 'last_nameが全角でなければ登録できない' do
+      end
+      it 'first_nameが全角でなければ登録できない' do
+      end
+      it 'last_name_kanaが全角カタカナでなければ登録できない' do
+      end
+      it 'first_name_kanaが全角カタカナでなければ登録できない' do
+      end
+      it 'birthdayが空では登録できない' do
+      end
     end
   end 
 end
