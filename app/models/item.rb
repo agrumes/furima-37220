28368bd6d6366917af4a1f77206d50d94b61488e
agrumes <gratitude2016@icloud.com>
2022-02-20 18:28:14 +0900

    Itemモデルと各ActiveHashモデルとのアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_day
 
-  
 end
