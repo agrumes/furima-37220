3f578021f9dc1273551273aff01985a9d02bc18c
agrumes <gratitude2016@icloud.com>
2022-02-21 18:38:13 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,12 +12,13 @@ class Item < ApplicationRecord
 
   validates :item_image, presence: true
   validates :item_name, presence: true, length: {maximum: 40 }
-  validates :tem_explanation, presence: true, length: {maximum: 1000 }
+  validates :item_explanation, presence: true, length: {maximum: 1000 }
   validates :category_id, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :delivery_charge_id, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :delivery_day_id, numericality: { other_than: 1 ,message: "can't be blank"}
-  validates :price, numericality: { in: 300..9999999 ,message: "Price is out of setting range"}
+  validates :price, presence: true, numericality: { in: 300..9999999 ,message: "out of setting range"},
+            format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
 
 end
