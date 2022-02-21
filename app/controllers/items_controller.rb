fb792ddf94b3115ea4793fdf55cfc51a79fdda46
agrumes <gratitude2016@icloud.com>
2022-02-21 18:10:01 +0900

    itemコントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      @item = Item.new(item_params)
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, :item_image).merge(user_id: current_user.id)
+  end
+
 end
