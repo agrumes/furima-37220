65bd2966314bcb4410ccb4b6f0adbe8f541cc4f6
agrumes <gratitude2016@icloud.com>
2022-02-23 21:08:22 +0900

    デバッグコード、コメントアウトの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,9 @@
 function commission (){
   const priceInput = document.getElementById("item-price");
-  // console.log(priceInput)
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    // console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-    // console.log(addTaxDom);
     const addProfit = document.getElementById("profit");
     const taxDom =  Math.floor(inputValue * 0.1);
     addProfit.innerHTML = Math.floor(inputValue - taxDom);
