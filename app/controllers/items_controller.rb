2ec260e03c5572d3a3afd2cf63340e61ba88f50a
agrumes <gratitude2016@icloud.com>
2022-02-23 13:34:12 +0900

    ログインの状態で出品ボタンの行先を変更するように記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_login, only: [:new, :create]
 
   def index
   end
@@ -20,6 +21,12 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:item_name, :item_explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, :item_image).merge(user_id: current_user.id)
+  end
+
+  def move_to_login
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
 end
