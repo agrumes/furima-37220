44c40e1b8b8480d4a5a2d831785584f908d21896
agrumes <gratitude2016@icloud.com>
2022-02-23 17:31:12 +0900

    exampleの追加、テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,45 +8,109 @@ RSpec.describe Item, type: :model do
   describe '新規商品出品' do
     context '商品が出品できる場合' do
       it '全ての項目が存在すれば登録できる' do
+        expect(@item).to be_valid
       end
       it 'priceが半角数字なら保存できる' do
+        @item.price = '1000'
+        expect(@item).to be_valid
       end
     end
 
     context '商品が出品できない場合' do
       it 'item_imageが空では保存できない' do
+        @item.item_image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item image can't be blank")
       end
       it 'item_nameが空では保存できない' do
+        @item.item_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
       it 'item_explanationが空では保存できない' do
+        @item.item_explanation = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item explanation can't be blank")
       end
       it 'category_idが空では保存できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'category_idが「1」では保存できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'condition_idが空では保存できない' do
+        @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it 'condition_idが「1」では保存できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it 'delivery_charge_idが空では保存できない' do
+        @item.delivery_charge_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
       it 'delivery_charge_idが「1」では保存できない' do
+        @item.delivery_charge_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
       it 'prefecture_idが空では保存できない' do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it 'prefecture_idが「1」では保存できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it 'delivery_day_idが空では保存できない' do
+        @item.delivery_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
       it 'delivery_day_idが「1」では保存できない' do
+        @item.delivery_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
       it 'priceが空では保存できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'priceが300未満では保存できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
       end
       it 'priceが999999999より大きい数字では保存できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
       end
       it 'priceが全角数字では保存できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters")
+      end
+      it 'priceが全角文字では保存できない' do
+        @item.price = 'ＴＥＳＴ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters")
+      end
+      it 'priceが半角文字では保存できない' do
+        @item.price = 'test'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters")
       end
     end
   end
