e28628d64c91606c7e6ac939f3d3be3724ed3cd9
agrumes <gratitude2016@icloud.com>
2022-02-25 21:15:33 +0900

    編集時もエラーメッセージが出るように記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,9 +29,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.save
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to action: :show
     else
       render :edit
