67b950f73c9a6edae1440bb97d2fecff07f23c59
agrumes <gratitude2016@icloud.com>
2022-03-01 10:19:50 +0900

    バリデーションの記述ミス修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,7 +8,7 @@ class Address < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city, presence: true
   validates :block, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true,
             format: { with: /\A\d{10,11}\z/, message: 'is too short' }
   validates :phone_number, 
             format: { with: /\A[0-9]+\z/, message: 'is invalid. Input only number' }
