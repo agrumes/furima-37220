3c519c45998b7ac43bfc053f2382cc8f4a168279
agrumes <gratitude2016@icloud.com>
2022-03-05 17:15:25 +0900

    FactoryBotを記述

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :purchase_address do
+    token                   { Faker::Internet.password(min_length: 20, max_length: 30)}
+    postal_code             { Faker::Number.decimal_part(digits: 3) + '-' +  Faker::Number.decimal_part(digits: 4) }
+    prefecture_id           { Faker::Number.between(from: 2, to: 48) }
+    city                    { Faker::Address.city }
+    block                   { Faker::Address.street_address }
+    building                { Faker::Address.street_address }
+    phone_number            { Faker::Number.decimal_part(digits: 11) }
+    item_id                 { Faker::Number.non_zero_digit }
+  end
+end
