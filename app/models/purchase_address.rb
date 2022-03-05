eb91e274f5ebe1b02c53c57c0426519b8d7ce581
agrumes <gratitude2016@icloud.com>
2022-03-05 17:41:21 +0900

    purchase_addressクラスへバリデーションの追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -8,12 +8,16 @@ class PurchaseAddress
 
   # with_optionsは複数のバリデーションをグループ化し、共通の制限をかけるためのメソッド
   with_options presence: true do
+  # addressモデルのバリデーション
   validates :token
   validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city
   validates :block
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is too short' }
+  # purchaseモデルのバリデーション
+  validates :user_id
+  validates :item_id
   end
 
   validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input only number' }
