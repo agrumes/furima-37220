b69cc2d6b87f4231aa9b2debb5aa0ca87517c9ff
agrumes <gratitude2016@icloud.com>
2022-03-21 16:37:35 +0900

    自動デプロイへ変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,27 @@
+#自動デプロイ
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+app_path = File.expand_path('../../../', __FILE__)  # 「../」が一つ増えている
+
+#アプリケーションサーバの性能を決定する
+worker_processes 1
+
+#アプリケーションの設置されているディレクトリを指定
+working_directory "#{app_path}/current"  # 「current」を指定
+
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更
+
+#ポート番号を指定
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"  # 「shared」の中を参照するよう変更
+
+#エラーのログを記録するファイルを指定
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"  # 「shared」の中を参照するよう変更
+
+#通常のログを記録するファイルを指定
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"  # 「shared」の中を参照するよう変更
+
+=begin
+#手動デプロイ
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../', __FILE__)
 
@@ -18,6 +42,7 @@ stderr_path "#{app_path}/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
 stdout_path "#{app_path}/log/unicorn.stdout.log"
+=end
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
